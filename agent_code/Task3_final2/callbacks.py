4fac4aea670b1da24a47d2e67df9204616ad0601
floern1 <florian.wk@web.de>
2021-03-27 10:50:01 +0100

    trained many models

---
+++ agent_code/Task3_final2/callbacks.py
@@ -0,0 +1,173 @@
+import os
+import pickle
+import random
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import os
+from random import shuffle
+
+from .Model import Maverick
+from .ManagerFeatures import *
+
+import events as e
+
+PARAMETERS = 'last_save' #select parameter_set stored in network_parameters/
+# PARAMETERS = 'save after 1998 iterations' #select parameter_set stored in network_parameters/
+# PARAMETERS = 'end_long_training' #select parameter_set stored in network_parameters/
+
+ACTIONS = ['LEFT', 'RIGHT', 'UP', 'DOWN', 'WAIT', 'BOMB']
+
+
+def setup(self):
+    """
+    This is called once when loading each agent.
+    Preperation such that act(...) can be called.
+
+    When in training mode, the separate `setup_training` in train.py is called
+    after this method.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    self.network = Maverick()
+
+    if self.train:
+        self.logger.info("Trainiere ein neues Model.")
+
+    else:
+        self.logger.info(f"Lade Model '{PARAMETERS}'.")
+        filename = os.path.join("network_parameters", f'{PARAMETERS}.pt')
+        self.network.load_state_dict(torch.load(filename))
+        self.network.eval()
+    
+    self.pos_saver_feat = []
+
+    self.bomb_timer = 0
+    initialize_rule_based(self)
+
+    self.bomb_buffer = 0
+    
+
+def act(self, game_state: dict) -> str:
+    """
+    Your agent should parse the input, think, and take a decision.
+    When not in training mode, the maximum execution time for this method is 0.5s.
+
+    :param self: The same object that is passed to all of your callbacks.
+    :param game_state: The dictionary that describes everything on the board.
+    :return: The action to take as a string.
+    """
+    
+    if game_state is None:
+        return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+
+    
+    if self.bomb_timer > 0:
+        self.bomb_timer -= 1
+
+    features = state_to_features(self, game_state)
+    Q = self.network(features)
+
+
+
+    if self.train: # Exploration vs exploitation
+        eps = self.epsilon_arr[self.episode_counter]
+        if random.random() <= eps: # choose random action
+            if eps > 0.1:
+                if np.random.randint(10) == 0:    # old: 10 / 100 now: 3/4
+                    action = np.random.choice(ACTIONS, p=[.167, .167, .167, .167, .166, .166])
+                    self.logger.info(f"Waehle Aktion {action} komplett zufaellig")
+
+                    if action == "BOMB" and self.bomb_timer==0:
+                        self.bomb_timer = 5
+                    return action
+
+                else:
+                    action = act_rulebased(self, features)
+                    if action == "BOMB" and self.bomb_timer==0:
+                        self.bomb_timer = 5
+                    self.logger.info(f"Waehle Aktion {action} nach dem rule based agent.")
+                    return action
+            else:
+                action = np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+                self.logger.info(f"Waehle Aktion {action} komplett zufaellig")
+
+                if action == "BOMB" and self.bomb_timer==0:
+                    self.bomb_timer = 5
+
+                return action
+
+    T = 1
+
+    action_prob	= np.array(torch.softmax(Q/T,dim=1).detach().squeeze())
+    best_action = ACTIONS[np.argmax(action_prob)]
+    # best_action = act_rulebased(self, features)
+    self.logger.info(f"Waehle Aktion {best_action} nach dem Hardmax der Q-Funktion")
+    if best_action == "BOMB" and self.bomb_timer==0:
+        self.bomb_timer = 5
+    return best_action
+
+
+def act_rulebased(self, features):
+
+    ACTIONS = ['RIGHT', 'LEFT', 'DOWN','UP',  'WAIT', 'BOMB']
+    
+    Q = np.dot(self.action_array, self.features)
+    action = ACTIONS[np.argmax(Q)]
+    # print()
+    # print(np.array([ACTIONS, Q]).T)
+    # print(f"--> {action}")
+    return action
+
+def initialize_rule_based(self):
+    self.action_array = np.zeros((6,23))
+    # coins
+    self.action_array[0][0] = 100
+    self.action_array[1][1] = 100
+    self.action_array[2][2] = 100
+    self.action_array[3][3] = 100
+
+    # crates
+    self.action_array[0][4] = 33
+    self.action_array[1][5] = 33
+    self.action_array[2][6] = 33
+    self.action_array[3][7] = 33
+
+    # bomb here
+    self.action_array[5][8] = 36
+
+    # explosion here
+    self.action_array[0][9] = 10
+    self.action_array[1][9] = 10
+    self.action_array[2][9] = 10
+    self.action_array[3][9] = 10
+    self.action_array[4][9] = -10
+    self.action_array[5][9] = -10
+
+    # run away
+    self.action_array[0][10] = 300
+    self.action_array[1][11] = 300
+    self.action_array[2][12] = 300
+    self.action_array[3][13] = 300
+    self.action_array[4][10] = 300
+    self.action_array[4][11] = 300
+    self.action_array[4][12] = 300
+    self.action_array[4][13] = 300
+    self.action_array[5][10] = 300
+    self.action_array[5][11] = 300
+    self.action_array[5][12] = 300
+    self.action_array[5][13] = 300
+
+    # not run in explosion
+    self.action_array[0][14] = 400
+    self.action_array[1][15] = 400
+    self.action_array[2][16] = 400
+    self.action_array[3][17] = 400
+
+    # walk to opponent
+    self.action_array[0][18] = 75
+    self.action_array[1][19] = 75
+    self.action_array[2][20] = 75
+    self.action_array[3][21] = 75
---
+++ agent_code/Task3_final_old/ManagerRewards.py
@@ -0,0 +1,53 @@
+import events as e
+
+def reward_from_events(self, events) -> int:
+
+    # game_rewards = {
+    #     e.COIN_COLLECTED: 500, #100,
+    #     e.KILLED_OPPONENT: 500,
+    #     e.MOVED_RIGHT: -1,
+    #     e.MOVED_LEFT: -1,
+    #     e.MOVED_UP: -1,
+    #     e.MOVED_DOWN: -1,
+    #     e.WAITED: -5,
+    #     e.INVALID_ACTION: -5,
+    #     e.BOMB_DROPPED: -1,
+    #     e.KILLED_SELF: -500,
+    #     e.GOT_KILLED: -500,
+    #     e.CRATE_DESTROYED: 100
+    # }
+    game_rewards = {
+        e.COIN_COLLECTED: 100,
+        e.KILLED_OPPONENT: 500,
+        e.MOVED_RIGHT: -1,
+        e.MOVED_LEFT: -1,
+        e.MOVED_UP: -1,
+        e.MOVED_DOWN: -1,
+        e.WAITED: -1,
+        e.INVALID_ACTION: -10,
+        e.BOMB_DROPPED: -1,
+        e.KILLED_SELF: 0,
+        e.GOT_KILLED: -700,
+    }
+    reward_sum = 0
+    for event in events:
+        if event in game_rewards:
+            reward_sum += game_rewards[event]
+    self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    return reward_sum
+
+def rewards_from_own_events(self, old_game_state, action, new_game_state, events):
+    reward_sum = 0
+
+    # check if agent moved closer to next coin
+    reward_sum += crate_rewards(self, events)
+
+    self.logger.info(f"Awarded {reward_sum} for own transition events")
+    return reward_sum
+
+# Give the crate rewards imediately after placing the crates. This makes our agent place bombs more often (better)
+def crate_rewards(self, events):
+    if e.BOMB_DROPPED in events:
+        self.logger.info(f"reward for the {self.destroyed_crates} that are going to be destroyed -> +{self.destroyed_crates * 33}")
+        return self.destroyed_crates * 33
+    return 0
